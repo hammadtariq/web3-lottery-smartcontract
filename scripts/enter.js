d021c6ca027220b5b79e803fe96589e703773e88
Hammad Tariq <htariq@nisum.com>
2022-07-28 15:28:21 +0500

    fully functional lottery contracts

---
+++ scripts/enter.js
@@ -0,0 +1,18 @@
+const { ethers } = require("hardhat")
+
+async function enterRaffle() {
+    const raffle = await ethers.getContract("Raffle")
+    const entranceFee = await raffle.getEntranceFee()
+    console.log("Entrance fee:", ethers.utils.formatEther(entranceFee))
+    const accounts = await ethers.getSigners()
+    console.log("Accounts:", accounts[0].address)
+    await raffle.enterRaffle({ value: entranceFee + 1 })
+    console.log("Entered!")
+}
+
+enterRaffle()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
